fe5fec575d45fcc5e29355b149e50d4d81c37b8a
Laurent Kretz <laurent.kretz@hotmail.fr>
2016-02-23 14:23:48 +0100

    routes update

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :perfumes, only: [:show, :index]
-  resources :kits, only: [:index, :show]
+  # resources :perfumes, only: [:show, :index]
+  # resources :kits, only: [:index, :show]
   resources :orders, only: [:create]
 
   namespace :checkout do
@@ -18,7 +18,7 @@ Rails.application.routes.draw do
     resources :orders, only: [:index]
     resources :kits, only: [:show]
     resources :perfumes, only: [:show] do
-      resources :reviews, only: [:create, :update, :destroy]
+      resources :reviews, only: [:create, :update]
     end
   end
 
