e7b98b548afb90b5aca531fbf4301279bacd14de
Laurent Kretz <laurent.kretz@hotmail.fr>
2016-02-23 16:16:13 +0100

    dashboard page created

--- app/controllers/account/dashboards_controller.rb
+++ app/controllers/account/dashboards_controller.rb
@@ -1,7 +1,11 @@
-class DashboardController < ApplicationController
+module Account
+  class DashboardsController < ApplicationController
+    skip_after_action :verify_authorized, only: :show
+    def show
+      @user = current_user
+      # @kit = @user.order_items.kit.order(created_at: "desc").first.product
+      # @perfumes = @kit.perfumes
+    end
 
-  def show
-    @user = record.user
   end
-
 end
