9e40e6f5017aed50fab5e9136029574f1625e010
Laurent Kretz <laurent.kretz@hotmail.fr>
2016-02-23 10:29:39 +0100

    added columns to user model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160223092656) do
+ActiveRecord::Schema.define(version: 20160223092910) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -108,6 +108,8 @@ ActiveRecord::Schema.define(version: 20160223092656) do
     t.datetime "updated_at",                          null: false
     t.string   "first_name"
     t.string   "last_name"
+    t.string   "address"
+    t.string   "address_details"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
