c3f584431b530ca570e9e157a1102f703b8f2663
Laurent Kretz <laurent.kretz@hotmail.fr>
2016-02-24 17:19:23 +0100

    invitable views generated

--- app/controllers/account/dashboards_controller.rb
+++ app/controllers/account/dashboards_controller.rb
@@ -3,6 +3,7 @@ module Account
     skip_after_action :verify_authorized, only: :show
     def show
       @user = current_user
+      @user.invite!
       @kit = @user.order_items.kit.order(created_at: "desc").first.product
       @perfumes = @kit.perfumes
     end
