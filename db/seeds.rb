c919bd2648560406ad0d3fe07989a411ab30a8f2
Laurent Kretz <laurent.kretz@hotmail.fr>
2016-02-24 11:44:45 +0100

    seed created

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,12 +5,13 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-  User.destroy_all
-  Order.destroy_all
-  Perfume.destroy_all
-  Kit.destroy_all
-  Sample.destroy_all
   OrderItem.destroy_all
+  Order.destroy_all
+  Address.destroy_all
+  User.destroy_all
+  Sample.destroy_all
+  Kit.destroy_all
+  Perfume.destroy_all
 
   jean=User.create!(first_name:"Jean", last_name:"Dupont", email:"jean.dupont@test.fr", password:"12345678")
   jean.addresses.create!(first_name:"Jean", last_name:"Dupont", street:"11 villa gaudelet", zip_code:"75011", city:"Paris")
@@ -25,18 +26,24 @@
   kit1=Kit.create!(name:"Collection 1", description:"Our first collection")
   sample1=perfume1.samples.create!
   sample1.kit=kit1
+  sample1.save
   sample2=perfume2.samples.create!
   sample2.kit=kit1
+  sample2.save
   sample3=perfume3.samples.create!
   sample3.kit=kit1
+  sample3.save
   sample4=perfume4.samples.create!
   sample4.kit=kit1
+  sample4.save
   sample5=perfume5.samples.create!
   sample5.kit=kit1
+  sample5.save
   kit1.save
   order_item1=OrderItem.create!(quantity:1, product_type:'Kit')
   order_item1.product = Kit.first
   order_item1.order = Order.first
+  order_item1.save
 
 
 
