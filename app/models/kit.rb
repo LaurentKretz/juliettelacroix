be4842453793220e15ead125484a666f6d74ef09
bertrand <bertrand.boullay@yahoo.fr>
2016-02-24 17:17:45 +0100

    discover page

--- app/models/kit.rb
+++ app/models/kit.rb
@@ -2,5 +2,11 @@ class Kit < ActiveRecord::Base
   has_many :order_items, as: :product
   has_many :samples
   has_many :perfumes, through: :samples
+
   validates :name, presence: :true
+
+  def self.current
+    # TODO: find the current kit
+    Kit.first
+  end
 end
