64d7e3155ed400778b9d1b298533e02486b663e5
Laurent Kretz <laurent.kretz@hotmail.fr>
2016-02-24 18:07:04 +0100

    homepage ask for invitation button

--- app/controllers/account/dashboards_controller.rb
+++ app/controllers/account/dashboards_controller.rb
@@ -3,7 +3,6 @@ module Account
     skip_after_action :verify_authorized, only: :show
     def show
       @user = current_user
-      @user.invite!
       @kit = @user.order_items.kit.order(created_at: "desc").first.product
       @perfumes = @kit.perfumes
     end
