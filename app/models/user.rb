3b84a3c485d94510f50d8c6c69b647b0940ff536
Laurent Kretz <laurent.kretz@hotmail.fr>
2016-02-24 16:09:16 +0100

    clean status

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :invitable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable,
+         :recoverable, :rememberable, :trackable, :validatable
   has_many :reviews
   has_many :orders
   has_many :addresses
