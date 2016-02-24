8c75c9d0783ddbecbd63363ec64c9a7a87dc6834
Laurent Kretz <laurent.kretz@hotmail.fr>
2016-02-24 14:29:22 +0100

    embedded vimeo video in dashboard

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,8 +1,8 @@
 class OrderItem < ActiveRecord::Base
   belongs_to :order
   belongs_to :product, polymorphic: :true
-  scope :kit, -> {where(product_type: "Kit")}
-  scope :kit, -> {where(product_type: "Perfume")}
   validates :order_id, presence: :true
   validates :product_id, presence: :true
+  scope :kit, -> {where(product_type: "Kit")}
+  scope :perfume, -> {where(product_type: "Perfume")}
 end
