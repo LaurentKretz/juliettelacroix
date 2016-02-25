a551e3aa7bf2ab0a1556bbcbe3833a84d3758903
bertrand <bertrand.boullay@yahoo.fr>
2016-02-25 16:08:54 +0100

    footer revamped

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
 ActiveRecord::Schema.define(version: 20160224163555) do
 
   # These are extensions that must be enabled in order to support this database
@@ -131,6 +130,7 @@ ActiveRecord::Schema.define(version: 20160224163555) do
     t.string   "first_name"
     t.string   "last_name"
     t.boolean  "admin",                  default: false, null: false
+    t.boolean  "first_purchase",         default: false
     t.string   "invitation_token"
     t.datetime "invitation_created_at"
     t.datetime "invitation_sent_at"
@@ -139,7 +139,6 @@ ActiveRecord::Schema.define(version: 20160224163555) do
     t.integer  "invited_by_id"
     t.string   "invited_by_type"
     t.integer  "invitations_count",      default: 0
-    t.boolean  "first_purchase",         default: false
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
