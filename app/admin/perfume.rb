a840c09ec330023da0990eb332e6458e089e7c55
Laurent Kretz <laurent.kretz@hotmail.fr>
2016-02-28 18:31:41 +0100

    updated perfume Winter Love info

--- app/admin/perfume.rb
+++ app/admin/perfume.rb
@@ -1,6 +1,6 @@
 ActiveAdmin.register Perfume do
 
-  permit_params :name, :description
+  permit_params :name, :description, :inspiration, :note1, :note2, :introduction
 
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
