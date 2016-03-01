bd416b4c70443f7298d0255ee83df871fbd65c97
bertrand <bertrand.boullay@yahoo.fr>
2016-03-01 09:23:03 +0100

    redesign dashboard

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -24,4 +24,9 @@ class ApplicationController < ActionController::Base
   end
 
   protect_from_forgery with: :exception
+
+  def after_sign_in_path_for(resource_or_scope)
+    account_dashboard_path
+  end
 end
+
