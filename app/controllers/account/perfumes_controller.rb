d4591805addf134c6355726ea21d988fadbfbc7e
Pierre-Henri Coustenoble <ph.coustenoble@gmail.com>
2016-03-02 12:53:27 +0100

    Remove steps test perfume

--- app/controllers/account/perfumes_controller.rb
+++ app/controllers/account/perfumes_controller.rb
@@ -9,42 +9,6 @@ module Account
       @perfumes = @kit.perfumes
     end
 
-    def step1
-      @user = current_user
-      @kit = @user.order_items.kit.order(created_at: "desc").first.product
-      @perfumes = @kit.perfumes
-    end
-
-    def step2
-      @user = current_user
-      @kit = @user.order_items.kit.order(created_at: "desc").first.product
-      @perfumes = @kit.perfumes
-    end
-
-    def step3
-      @user = current_user
-      @kit = @user.order_items.kit.order(created_at: "desc").first.product
-      @perfumes = @kit.perfumes
-    end
-
-    def step4
-      @user = current_user
-      @kit = @user.order_items.kit.order(created_at: "desc").first.product
-      @perfumes = @kit.perfumes
-    end
-
-    def step5
-      @user = current_user
-      @kit = @user.order_items.kit.order(created_at: "desc").first.product
-      @perfumes = @kit.perfumes
-    end
-
-    def step6
-      @user = current_user
-      @kit = @user.order_items.kit.order(created_at: "desc").first.product
-      @perfumes = @kit.perfumes
-    end
-
     private
 
     def set_perfume
