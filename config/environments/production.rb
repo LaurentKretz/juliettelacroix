be659296fbccb36cfeec3762dc60d354ae3d4e63
Laurent Kretz <laurent.kretz@hotmail.fr>
2016-03-03 16:36:55 +0100

    mailer host seeted to juliettelacroix.fr

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,6 @@
 Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: 'juliettelacroix-lewagon.herokuapp.com' }
+  config.action_mailer.default_url_options = { host: 'www.juliettelacroix.fr' }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
